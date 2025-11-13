a21209edac2ded842022909a7ddba21126f7372c
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-11-13 20:12:32 +0200

    fix: bodyLock

--- src/js/function/bodyLock.js
+++ src/js/function/bodyLock.js
@@ -1,46 +1,64 @@
 export let bodyLockStatus = true;
-export let bodyLockToggle = (delay = 500) => {
-  if (document.documentElement.hasAttribute('data-scroll-lock')) {
-    bodyUnlock(delay);
-  } else {
-    bodyLock(delay);
-  }
-};
-export let bodyUnlock = (delay = 500) => {
-  if (bodyLockStatus) {
-    const lockPaddingElements = document.querySelectorAll(
-      '[data-right-padding]',
-    );
-    setTimeout(() => {
-      lockPaddingElements.forEach((lockPaddingElement) => {
-        lockPaddingElement.style.paddingRight = '';
-      });
-      document.body.style.paddingRight = '';
-      document.documentElement.removeAttribute('data-scroll-lock');
-    }, delay);
-    bodyLockStatus = false;
-    setTimeout(function () {
-      bodyLockStatus = true;
-    }, delay);
-  }
-};
-export let bodyLock = (delay = 500) => {
-  if (bodyLockStatus) {
-    const lockPaddingElements = document.querySelectorAll(
-      '[data-right-padding]',
-    );
-    const lockPaddingValue =
-      window.innerWidth - document.body.offsetWidth + 'px';
-    lockPaddingElements.forEach((lockPaddingElement) => {
-      lockPaddingElement.style.paddingRight = lockPaddingValue;
-    });
 
-    document.body.style.paddingRight = lockPaddingValue;
-    document.documentElement.setAttribute('data-scroll-lock', '');
+const setBodyLockStatus = (delay) => {
+  bodyLockStatus = false;
+  setTimeout(() => {
+    bodyLockStatus = true;
+  }, delay);
+};
 
-    bodyLockStatus = false;
-    setTimeout(function () {
-      bodyLockStatus = true;
-    }, delay);
+const getLockPaddingElements = () =>
+  document.querySelectorAll('[data-right-padding]');
+
+const getScrollbarWidthRem = () => {
+  const scrollbarWidthPx = window.innerWidth - document.body.clientWidth;
+  return scrollbarWidthPx / 16 + 'rem';
+};
+
+const setPadding = (value = '') => {
+  const lockPaddingElements = getLockPaddingElements();
+  lockPaddingElements.forEach((el) => {
+    el.style.paddingRight = value;
+  });
+  document.body.style.paddingRight = value;
+};
+
+const setScrollbarWidthVariable = (value) => {
+  document.documentElement.style.setProperty('--scrollbar-width', value);
+};
+
+const clearScrollbarWidthVariable = () => {
+  document.documentElement.style.removeProperty('--scrollbar-width');
+};
+
+export const bodyLock = (delay = 500) => {
+  if (!bodyLockStatus) {
+    return;
   }
+
+  const scrollbarWidthRem = getScrollbarWidthRem();
+  setPadding(scrollbarWidthRem);
+  setScrollbarWidthVariable(scrollbarWidthRem);
+
+  document.documentElement.setAttribute('data-scroll-lock', '');
+  setBodyLockStatus(delay);
+};
+
+export const bodyUnlock = (delay = 500) => {
+  if (!bodyLockStatus) {
+    return;
+  }
+
+  setTimeout(() => {
+    setPadding('');
+    clearScrollbarWidthVariable();
+    document.documentElement.removeAttribute('data-scroll-lock');
+  }, delay);
+
+  setBodyLockStatus(delay);
+};
+
+export const bodyLockToggle = (delay = 500) => {
+  const isLocked = document.documentElement.hasAttribute('data-scroll-lock');
+  isLocked ? bodyUnlock(delay) : bodyLock(delay);
 };
